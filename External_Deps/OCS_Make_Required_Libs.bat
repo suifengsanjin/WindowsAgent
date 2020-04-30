ecf54725c61f371b88c753fdfffed7376790198c
H2102471 <H2102471@foxconn.com>
2020-04-30 15:46:04 +0800

    1 modify project cfg 2 mod deps building bat

--- External_Deps/OCS_Make_Required_Libs.bat
+++ External_Deps/OCS_Make_Required_Libs.bat
@@ -13,39 +13,45 @@ echo.
 Rem ========= UPDATE CONSTANTS BELOW TO MEET YOUR CONFIGURATION NEED =========  
 
 Rem Set path to MS Visual C++
-set VC_PATH=C:\Program Files (x86)\\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+set VC_PATH=D:\soft\\Microsoft Visual Studio 12.0\VC
 
 Rem Set path to MS Windows SDK, needed to build cURL
-set WINDOWS_SDK_PATH="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A"
+set WINDOWS_SDK_PATH="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A"
 
 Rem Set path to Perl 5.6 or higher binary
-set PERL_PATH=C:\Strawberry\perl\bin
+set PERL_PATH=D:\soft\perl\perl\bin
 
 Rem Set path to Zlib sources
-set ZLIB_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\zlib-1.2.8
+set ZLIB_PATH=D:\src\ocs\WindowsAgent\External_Deps\zlib
 
 Rem Set path to OpenSSL sources
-set OPENSSL_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\openssl-1.0.2r
+set OPENSSL_PATH=D:\src\ocs\WindowsAgent\External_Deps\openssl
 
 Rem Set path to cURL sources
-set CURL_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\curl-7.64.1\src
+set CURL_PATH=D:\src\ocs\WindowsAgent\External_Deps\curl
 
 Rem Set path to tinyXML sources
-SET XML_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\tinyxml
+SET XML_PATH=D:\src\ocs\WindowsAgent\External_Deps\tinyxml
 
 Rem Set path to ZipArchive sources, for example
-SET ZIP_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\ZipArchive
+SET ZIP_PATH=D:\src\ocs\WindowsAgent\External_Deps\ZipArchive\ZipArchive
 
 Rem Set path to Net-SNMP sources, for example
-SET SNMP_PATH=C:\Users\Admin\Documents\GitHub\WindowsAgent\External_Deps\net-snmp-5.7.3
+SET SNMP_PATH=D:\src\ocs\WindowsAgent\External_Deps\net-snmp-5.7.3
 
 Rem ========= DO NOT MODIFY BELOW, UNTIL YOU KNOW WHAT YOU ARE DOING =========
 
 Rem Ensure MS Visual C++ environnement is set
-call "%VC_PATH%\VCVARSALL.BAT" x86
+call "%VC_PATH%\VCVARSALL.BAT"
 Rem Add perl to PATH
 set PATH=%PATH%;%PERL_PATH%
 
+mkdir %~dp0\Release
+mkdir %~dp0\Debug
+mkdir %~dp0\Include
+
+Rem goto comment
+Rem goto incFile
 title OCS Inventory NG Agent for Windows - Building Zlib DLL...
 echo.
 echo *************************************************************************
@@ -57,19 +63,17 @@ echo.
 cd "%ZLIB_PATH%"
 
 Rem Build Zlib using precompiled asm code for MS Visual C++ with lastest Service Pack ( -D_BIND_TO_CURRENT_VCLIBS_VERSION)
-nmake /NOLOGO -f Win32\Makefile.msc LOC="-DASMV -DASMINF -D_BIND_TO_CURRENT_VCLIBS_VERSION" OBJA="inffas32.obj match686.obj"
+nmake /NOLOGO -f win32/Makefile.msc clean
+nmake /NOLOGO -f win32\Makefile.msc LOC="-DASMV -DASMINF -D_BIND_TO_CURRENT_VCLIBS_VERSION" OBJA="inffas32.obj match686.obj"
 if ERRORLEVEL 1 goto ERROR
 
 Rem copy libs to use them in OCS
-copy zdll.lib ..
-mkdir ..\..\Release
-copy zlib1.dll ..\..\Release
-mkdir ..\..\Debug
-copy zlib1.dll ..\..\Debug
+copy zdll.lib %~dp0
+copy zlib1.dll %~dp0\Release
+copy zlib1.dll %~dp0\Debug
 if ERRORLEVEL 1 goto ERROR
 
-cd ..
-
+:comment
 title OCS Inventory NG Agent for Windows - Building OpenSSL DLLs...
 echo.
 echo *************************************************************************
@@ -93,23 +97,21 @@ nmake /NOLOGO -f ms\ntdll.mak
 if ERRORLEVEL 1 goto ERROR
 Rem Test OpenSSL build
 cd out32dll
-call "..\ms\test.bat"
+call "%OPENSSL_PATH%\ms\test.bat"
 if ERRORLEVEL 1 goto ERROR
 
 Rem copy libs to use them in OCS
-copy ssleay32.lib ..\..
-copy libeay32.lib ..\..
-copy libeay32.lib ..\..\curl-7.64.1\winbuild
-copy ssleay32.lib ..\..\curl-7.64.1\winbuild
-copy ssleay32.dll ..\..\..\Release
-copy libeay32.dll ..\..\..\Release
-copy ssleay32.dll ..\..\..\Debug
-copy libeay32.dll ..\..\..\Debug
+copy ssleay32.lib %~dp0
+copy libeay32.lib %~dp0
+copy libeay32.lib %CURL_PATH%\winbuild
+copy ssleay32.lib %CURL_PATH%\winbuild
+copy ssleay32.dll %~dp0\Release
+copy libeay32.dll %~dp0\Release
+copy ssleay32.dll %~dp0\Debug
+copy libeay32.dll %~dp0\Debug
 if ERRORLEVEL 1 goto ERROR
 
-cd ..\..
-
-
+:curl
 title OCS Inventory NG Agent for Windows - Building cURL DLL...
 echo.
 echo *************************************************************************
@@ -118,29 +120,32 @@ echo * Preparing for OCS Inventory NG : Building cURL DLL...                 *
 echo *                                                                       *
 echo *************************************************************************
 echo.
-cd "%CURL_PATH%"
+mkdir %~dp0\deps
+mkdir %~dp0\deps\bin
+mkdir %~dp0\deps\lib
+mkdir %~dp0\deps\include
+xcopy /Y %OPENSSL_PATH%\inc32\openssl %~dp0\deps\include\openssl\
+cd "%CURL_PATH%"\src
 Rem Disable LDAP support, not needed in OCS Inventory NG Agent
 set WINDOWS_SSPI=0
-cd ..\winbuild
+cd %CURL_PATH%\winbuild
 Rem Fix cURL DLL config for MS Visual C++ with lastest Service Pack ( -D_BIND_TO_CURRENT_VCLIBS_VERSION)
 Rem perl.exe -pi.bak -e "s# /DBUILDING_LIBCURL# /DBUILDING_LIBCURL /D_BIND_TO_CURRENT_VCLIBS_VERSION#g" Makefile.vc12
 Rem Build cURL dll using OpenSSL Dlls and Zlib dll
 Rem nmake /NOLOGO /f Makefile.vc12 cfg=release-dll-ssl-dll-zlib-dll
-nmake /f Makefile.vc mode=dll VC=15 ENABLE_SSPI=NO ENABLE_IPV6=YES WITH_SSL=dll MACHINE=x86 SSL_PATH="%OPENSSL_PATH%\inc32" ZLIB_PATH="%ZLIB_PATH%"
-
+nmake /f Makefile.vc mode=dll VC=12 ENABLE_SSPI=NO ENABLE_IPV6=YES WITH_SSL=dll
 if ERRORLEVEL 1 goto ERROR
-Rem Insert manifest into DLL
-rem cd ..\lib\release-dll-ssl-dll-zlib-dll
-rem mt -manifest libcurl.dll.manifest -outputresource:libcurl.dll;2
-if ERRORLEVEL 1 goto ERROR
+rmdir /S /Q %~dp0\deps
+echo Building cURL DLLs Over and Copy Dll Begin...
 
 Rem copy libs to use them in OCS
-copy "..\builds\libcurl-vc15-x86-release-dll-ssl-dll-obj-lib\libcurl.lib" ..\..
-copy "..\builds\libcurl-vc15-x86-release-dll-ssl-dll-obj-lib\libcurl.dll" ..\..\..\Release
-copy "..\builds\libcurl-vc15-x86-release-dll-ssl-dll-obj-lib\libcurl.dll" ..\..\..\Debug
+copy "%CURL_PATH%\builds\libcurl-vc12-x86-release-dll-ssl-dll-obj-lib\libcurl.lib" %~dp0
+copy "%CURL_PATH%\builds\libcurl-vc12-x86-release-dll-ssl-dll-obj-lib\libcurl.dll" %~dp0\Release
+copy "%CURL_PATH%\builds\libcurl-vc12-x86-release-dll-ssl-dll-obj-lib\libcurl.dll" %~dp0\Debug
 if ERRORLEVEL 1 goto ERROR
 
-cd ..\..\..
+echo Copy cURL DLLs Over...
+
 
 title OCS Inventory NG Agent for Windows - Building Net-SNMP DLL...
 echo.
@@ -155,27 +160,23 @@ Rem Prepare OpenSSL for Net-SNMP
 SET INCLUDE=%INCLUDE%;%OPENSSL_PATH%\inc32
 SET LIB=%LIB%;%OPENSSL_PATH%\out32dll
 Rem Configure Net-SNMP
-perl.exe Configure --config=release --with-ssl --linktype=dynamic
+perl.exe Configure  --with-ssl --linktype=dynamic
 if ERRORLEVEL 1 goto ERROR
 cd libsnmp_dll
 Rem Fix Net-SNMP DLL config for MS Visual C++ with lastest Service Pack ( -D_BIND_TO_CURRENT_VCLIBS_VERSION)
 perl.exe -pi.bak -e "s# /D \"WIN32\"# /D \"WIN32\" /D_BIND_TO_CURRENT_VCLIBS_VERSION#g" Makefile
 Rem Fix Net-SNMP DLL config for using OpenSSL dynamic library insteadd of static ones
 perl.exe -pi.bak -e "s#libeay32MD#libeay32#g" ../net-snmp/net-snmp-config.h
-Rem Fix Net-SNMP DLL config for compile net-snmp with VS2017 
-perl.exe -pi.bak -e "s#define snprintf  _snprintf##g" ../net-snmp/net-snmp-config.h
-
 Rem Build Net-SNMP dll
 nmake /NOLOGO
 if ERRORLEVEL 1 goto ERROR
-cd ..
-copy "bin\release\netsnmp.dll" ..\..\..\Release
-copy "bin\release\netsnmp.dll" ..\..\..\Debug
+copy "%SNMP_PATH%\win32\bin\release\netsnmp.dll" %~dp0\Release
+copy "%SNMP_PATH%\win32\bin\release\netsnmp.dll" %~dp0\Debug
 if ERRORLEVEL 1 goto ERROR
-copy "lib\release\netsnmp.lib" ..\..
+copy "%SNMP_PATH%\win32\lib\release\netsnmp.lib" %~dp0
 if ERRORLEVEL 1 goto ERROR
 
-cd ..\..
+
 title OCS Inventory NG Agent for Windows - Building ZipArchive DLL...
 echo.
 echo *************************************************************************
@@ -186,12 +187,13 @@ echo *************************************************************************
 echo.
 cd "%ZIP_PATH%\Release Unicode STL MD DLL"
 Rem copy libs to use them in OCS
-copy "ZipArchive.lib" ..\..
-copy "ZipArchive.dll" ..\..\..\Release
-copy "ZipArchive.dll" ..\..\..\Debug
+copy "ZipArchive.lib" %~dp0
+rem copy "ZipArchive.dll" %~dp0\Release
+rem copy "ZipArchive.dll" %~dp0\Debug
 if ERRORLEVEL 1 goto ERROR
 
-cd ..\..
+:errMsg
+cd %~dp0
 title OCS Inventory NG Agent for Windows - Building service message file...
 echo.
 echo *************************************************************************
@@ -204,7 +206,15 @@ cd "..\Service"
 mc.exe NTServiceMsg.mc
 if ERRORLEVEL 1 goto ERROR
 
-cd ..\External_Deps
+:incFile
+xcopy /Y %OPENSSL_PATH%\inc32\openssl %~dp0\Include\openssl\
+xcopy /Y %CURL_PATH%\include\curl %~dp0\Include\curl\
+copy %ZLIB_PATH%\zlib.h %~dp0\Include\
+copy %ZLIB_PATH%\zconf.h %~dp0\Include\
+
+copy %ZIP_PATH%\*.h %~dp0\Include\*.h
+
+cd %~dp0
 title OCS Inventory NG Agent for Windows - SUCCESSFUL build of required libraries 
 echo.
 echo *************************************************************************
